c093b44a57591dba2a33a49eee09c3f1d4b00903
Dan golan <dagolan@g.jct.ac.il>
2022-10-23 23:39:14 +0300

    vv

--- Stage0/Program0871.cs
+++ Stage0/Program0871.cs
@@ -10,7 +10,7 @@ partial class Program
             Welcome0871();
             Welcome2444();
             Console.ReadKey();
-            //////ppppp
+           
 
         }
         static partial void Welcome2444();
