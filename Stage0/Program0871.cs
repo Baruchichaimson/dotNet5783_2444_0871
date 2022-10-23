bf141dbedc6313fcd844677c7be03aaf321d7672
Dan golan <dagolan@g.jct.ac.il>
2022-10-23 21:59:59 +0300

    welcome

--- Stage0/Program0871.cs
+++ Stage0/Program0871.cs
@@ -1,2 +1,25 @@
 ﻿using System;
 
+namespace  Program0871
+{
+partial class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Welcome0871();
+            Welcome2444();
+            Console.ReadKey();
+
+        }
+        static partial void Welcome2444();
+        private static void Welcome0871()
+        {
+            Console.Write("Enter your name: ");
+            string username = Console.ReadLine();
+            Console.WriteLine("{0} , welcome to first console project", username);
+        }
+    }
+
+
+}
