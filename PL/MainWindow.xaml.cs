9a07b955e78ad185bb67e8379de4135b12d9dc8d
Dan golan <dagolan@g.jct.ac.il>
2022-12-10 22:59:09 +0200

    logo folder

--- PL/MainWindow.xaml.cs
+++ PL/MainWindow.xaml.cs
@@ -3,6 +3,8 @@ using BlImplementation;
 using PL.product_main_windows;
 using System;
 using System.Collections.Generic;
+using System.DirectoryServices;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,9 +25,13 @@ namespace PL
     public partial class MainWindow : Window
     {
         private IBl bl = new Bl();
+        public ImageSource background { set; get; }
         public MainWindow()
         {
+           // string s = Directory.GetCurrentDirectory();
+           // background = new BitmapImage(new Uri(@$"{s}\images\Logo.png"));
             InitializeComponent();
+           
         }
         private void ShowProductButton_Click(object sender, RoutedEventArgs e) => new ProductList().Show();
     }
