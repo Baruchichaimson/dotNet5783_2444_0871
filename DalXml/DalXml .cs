fa3baf09d2121e0d7ac3b3c17839796bf91ddea8
Baruchichaimson <115399723+Baruchichaimson@users.noreply.github.com>
2023-01-14 23:44:48 +0200

    remakes

--- DalXml/DalXml .cs
+++ DalXml/DalXml .cs
@@ -3,6 +3,10 @@ using DalApi;
 
 namespace Dal
 {
+    /// <summary>
+    /// The DalXml class is an implementation of the IDal interface that uses XML files to store data.
+    /// It contains instances of the DalProduct, DalOrder, and DalOrderItem classes for managing products, orders, and order items respectively.
+    /// </summary>
     internal sealed class DalXml : IDal
     {
         public static IDal Instance { get; } = new DalXml();
